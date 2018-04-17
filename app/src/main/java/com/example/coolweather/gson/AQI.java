fdafcd9b114d0fba028f2e636cd8f3d6dcc73984
unknown <1457406681@qq.com>
2018-04-17 16:29:59 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by yudi on 2018/3/30.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
--- app/src/main/java/com/example/coolweather/service/ChooseAreaFragment.java
+++ app/src/main/java/com/example/coolweather/service/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.coolweather.service;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,9 +18,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.coolweather.R;
+import com.example.coolweather.WeatherActivity;
 import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
 import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
 
@@ -112,6 +115,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
